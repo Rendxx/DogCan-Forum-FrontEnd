6c6e55f83efaf58d65455c61d41a628bbc344973
Rendxx <rendxx@gmail.com>
2015-11-16 18:18:06 -0600

    sample forum done

---
+++ DogCan-Forum-Part/src/js/forum/layout.js
@@ -0,0 +1,29 @@
+﻿window.DogCan = window.DogCan || {};
+window.DogCan.Forum = window.DogCan.Forum || {};
+
+// manage forum layout on scrolling
+window.DogCan.Forum.Layout = function () {
+    var that = this;
+    var html = {};
+
+    var setupHtml = function () {
+        html['header'] = $('.block-header');
+        html['list'] = $('.block-list');
+    };
+
+    var setupFunc = function () {
+        $$.scroll.set(html['list'][0], 200, function () {
+            html['header'].addClass('fixed_');
+        }, $$.scroll.REFERENCE.TOP, $$.scroll.DIRECTION.DOWN);
+
+        $$.scroll.set(html['list'][0], 200, function () {
+            html['header'].removeClass('fixed_');
+        }, $$.scroll.REFERENCE.TOP, $$.scroll.DIRECTION.UP);
+    };
+
+    var _init = function () {
+        setupHtml();
+        setupFunc();
+    };
+    _init();
+};
